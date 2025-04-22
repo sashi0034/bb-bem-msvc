1569c1754bf8b16c09af86b74856bf5061e1fa18
sashi <satoshi3104.nishi@gmail.com>
2025-04-23 08:17:52 +0900

    テクスチャ作成

--- Viewer_s3d/Main.cpp
+++ Viewer_s3d/Main.cpp
@@ -1,22 +1,38 @@
 ﻿# include <Siv3D.hpp>
 
+namespace
+{
+	RenderTexture getPlaneTexture() {
+		RenderTexture texture{512, 512, TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::No, HasMipMap::Yes};
+		const ScopedRenderTarget2D rt{texture.clear(ColorF{0.9}.removeSRGBCurve())};
+		constexpr int step_32 = 32;
+		for (int x = 0; x < texture.width(); x += step_32) {
+			for (int y = 0; y < texture.height(); y += step_32) {
+				RectF{Vec2{x, y}, Vec2{step_32, step_32}}.drawFrame(1.0, ColorF{0.7}.removeSRGBCurve());
+			}
+		}
+
+		return texture;
+	}
+}
+
 struct Viewer_3sd : IAddon {
 	// 背景色 (リニアレンダリング用なので removeSRGBCurve() で sRGB カーブを除去）
-	ColorF backgroundColor = ColorF{0.4, 0.6, 0.8}.removeSRGBCurve();
+	ColorF m_backgroundColor = ColorF{0.5}.removeSRGBCurve();
 
 	// UV チェック用テクスチャ (ミップマップ使用。リニアレンダリング時に正しく扱われるよう、sRGB テクスチャであると明示）
-	Texture uvChecker{U"example/texture/uv.png", TextureDesc::MippedSRGB};
+	RenderTexture m_planeTexture{getPlaneTexture()};
 
 	// 3D シーンを描く、マルチサンプリング対応レンダーテクスチャ
 	// リニア色空間のレンダリング用に TextureFormat::R8G8B8A8_Unorm_SRGB
 	// 奥行きの比較のための深度バッファも使うので HasDepth::Yes
 	// マルチサンプル・レンダーテクスチャなので、描画内容を使う前に resolve() が必要
-	MSRenderTexture renderTexture{Scene::Size(), TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes};
+	MSRenderTexture m_renderTexture{Scene::Size(), TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes};
 
 	// 3D シーンのデバッグ用カメラ
 	// 縦方向の視野角 30°, カメラの位置 (10, 16, -32)
 	// 前後移動: [W][S], 左右移動: [A][D], 上下移動: [E][X], 注視点移動: アローキー, 加速: [Shift][Ctrl]
-	DebugCamera3D camera{renderTexture.size(), 30_deg, Vec3{10, 16, -32}};
+	DebugCamera3D m_camera{m_renderTexture.size(), 30_deg, Vec3{10, 16, -32}};
 
 	bool init() override {
 		// ウインドウとシーンを 1280x720 にリサイズ
@@ -27,19 +43,19 @@ struct Viewer_3sd : IAddon {
 
 	bool update() override {
 		// デバッグカメラの更新 (カメラの移動スピード: 2.0)
-		camera.update(2.0);
+		m_camera.update(2.0);
 
 		// 3D シーンにカメラを設定
-		Graphics3D::SetCameraTransform(camera);
+		Graphics3D::SetCameraTransform(m_camera);
 
 		// 3D 描画
 		{
 			// renderTexture を背景色で塗りつぶし、
 			// renderTexture を 3D 描画のレンダーターゲットに
-			const ScopedRenderTarget3D target{renderTexture.clear(backgroundColor)};
+			const ScopedRenderTarget3D target{m_renderTexture.clear(m_backgroundColor)};
 
 			// 床を描画
-			Plane{64}.draw(uvChecker);
+			Plane{64}.draw(m_planeTexture);
 
 			// ボックスを描画
 			Box{-8, 2, 0, 4}.draw(ColorF{0.8, 0.6, 0.4}.removeSRGBCurve());
@@ -57,10 +73,10 @@ struct Viewer_3sd : IAddon {
 			Graphics3D::Flush();
 
 			// マルチサンプル・テクスチャのリゾルブ
-			renderTexture.resolve();
+			m_renderTexture.resolve();
 
 			// リニアレンダリングされた renderTexture をシーンに転送
-			Shader::LinearToScreen(renderTexture);
+			Shader::LinearToScreen(m_renderTexture);
 		}
 
 		return true;
